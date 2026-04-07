9b0a50f6174b65535c01014d0a0ffeac0bb315c2
priyanshishakya <princyshakya246@gmail.com>
2026-04-08 00:28:59 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -1,41 +1,42 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
         int n = sc.nextInt();
         
         int[][] matrix = new int[n][n];
         int num = 1;
         
-       
         int top = 0, bottom = n - 1, left = 0, right = n - 1;
         
         while (num <= n * n) {
-          
+            
             for (int col = left; col <= right && num <= n * n; col++) {
                 matrix[top][col] = num++;
             }
             top++;
             
-           
+            
             for (int row = top; row <= bottom && num <= n * n; row++) {
                 matrix[row][right] = num++;
             }
             right--;
             
-            
+           
             for (int col = right; col >= left && num <= n * n; col--) {
                 matrix[bottom][col] = num++;
             }
             bottom--;
             
-            
+           
             for (int row = bottom; row >= top && num <= n * n; row--) {
                 matrix[row][left] = num++;
             }
             left++;
         }
         
-       
+      
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(matrix[i][j]);
@@ -54,7 +55,6 @@ public class Main {
         sc.close();
     }
 }
-
         // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
         //       Print each row with values separated by single space
         //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
