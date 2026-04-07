3ffac17c76f5dab4c172415a22020a3b4776cee5
priyanshishakya <princyshakya246@gmail.com>
2026-04-08 00:07:42 +0530

    Update Main.java

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -3,6 +3,40 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String[] tokens = line.split(" ");
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = applyOperator(token, a, b);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        
+        System.out.println(stack.pop());
+        sc.close();
+    }
+    
+    private static boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+    
+    private static int applyOperator(String operator, int a, int b) {
+        switch (operator) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b;
+            default: throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+}
 
         // TODO: Evaluate a postfix (Reverse Polish Notation) expression
         //       Operands are non-negative integers, operators are: + - * /
@@ -15,7 +49,4 @@ public class Main {
         //
         // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
 
-        String line = sc.nextLine();
-
-    }
-}
+       
