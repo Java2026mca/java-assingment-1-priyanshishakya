c1d861647ad88b1cc4cd8af42bb24a16b5f02314
priyanshishakya <princyshakya246@gmail.com>
2026-04-08 00:24:47 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,29 +5,31 @@ public class Main {
         
         int[][] matrix = new int[n][n];
         int num = 1;
-        int top = 0, bottom = n-1, left = 0, right = n-1;
         
-     
-        while (num <= n*n) {
-            for (int col = left; col <= right && num <= n*n; col++) {
+       
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        
+        while (num <= n * n) {
+          
+            for (int col = left; col <= right && num <= n * n; col++) {
                 matrix[top][col] = num++;
             }
             top++;
             
-            
-            for (int row = top; row <= bottom && num <= n*n; row++) {
+           
+            for (int row = top; row <= bottom && num <= n * n; row++) {
                 matrix[row][right] = num++;
             }
             right--;
             
             
-            for (int col = right; col >= left && num <= n*n; col--) {
+            for (int col = right; col >= left && num <= n * n; col--) {
                 matrix[bottom][col] = num++;
             }
             bottom--;
             
-        
-            for (int row = bottom; row >= top && num <= n*n; row--) {
+            
+            for (int row = bottom; row >= top && num <= n * n; row--) {
                 matrix[row][left] = num++;
             }
             left++;
@@ -39,7 +39,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(matrix[i][j]);
-                if (j < n-1) System.out.print(" ");
+                if (j < n - 1) System.out.print(" ");
             }
             System.out.println();
         }
@@ -54,3 +54,14 @@ public class Main {
         sc.close();
     }
 }
+
+        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
+        //       Print each row with values separated by single space
+        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
+        //
+        // Input: 3
+        // Output:
+        // 1 2 3
+        // 8 9 4
+        // 7 6 5
+        // Diagonal: 15
