8567e11231f606a186bac46af3cdb045baa60e2b
priyanshishakya <princyshakya246@gmail.com>
2026-04-07 23:58:29 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -3,15 +3,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt()
+        int n = sc.nextInt();  // ✅ Added semicolon
+        
         // Upper part
         for (int i = 1; i <= n; i++) {
-
             // spaces before
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-
+            
             // hollow stars
             for (int j = 1; j <= 2 * i - 1; j++) {
                 if (j == 1 || j == 2 * i - 1) {
@@ -20,17 +20,18 @@ public class Main {
                     System.out.print(" ");
                 }
             }
-
+            
             System.out.println();
         }
-
+        
         // Lower part
         for (int i = n - 1; i >= 1; i--) {
-
+            // spaces before
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-
+            
+            // hollow stars
             for (int j = 1; j <= 2 * i - 1; j++) {
                 if (j == 1 || j == 2 * i - 1) {
                     System.out.print("*");
@@ -38,12 +39,13 @@ public class Main {
                     System.out.print(" ");
                 }
             }
-
+            
             System.out.println();
         }
+        
+        sc.close();  
     }
-}
-            
+}        
      
     
         
@@ -56,6 +58,3 @@ public class Main {
         //  *   *
         //   * *
         //    *
-
-    }
-}
