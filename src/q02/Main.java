8d8419f145792a29469e81db0fc9bc59689691a9
priyanshishakya <princyshakya246@gmail.com>
2026-04-07 23:46:58 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -5,23 +5,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-      
         for (int row = 0; row < n; row++) {
-            int prevAboveLeft = 1;
-            int prevAboveRight = 1;
-            
-         
+            // Print left 1
             System.out.print("1");
             
-           
+            // Print middle using binomial coefficient property
             for (int j = 1; j < row; j++) {
-                int current = prevAboveLeft + prevAboveRight;
+                // C(row, j) = row! / (j! * (row-j)!)
+                // Use multiplicative formula to avoid overflow
+                long current = 1L * (row - j + 1) / j * prev;
                 System.out.print(" " + current);
-                prevAboveLeft = prevAboveRight;
-                prevAboveRight = current;
+                prev = current;
             }
             
-          
+            // Print right 1
             if (row > 0) {
                 System.out.print(" 1");
             }
